3c590fcb74aa8763ce3345edb715cd2e40154a1b
TJadeUQ <37641472+TJadeUQ@users.noreply.github.com>
2018-04-04 18:24:36 +1000

    Add files via upload

--- MassDistrEach.py
+++ MassDistrEach.py
@@ -10,8 +10,7 @@ import matplotlib.colors
 import math
 #Staying in the positive quadrant
 #Location of the masses
-mr = ((100,200),(300,100))
-
+mr = ((10,20),(30,10))
 k = ([10000, 20000, 30000, 40000])
 #Distance to the masses ri
 ri = np.array([])
@@ -19,53 +18,69 @@ for i in range(len(mr[0])):
     for j in range(len(mr[1])):
         rad = ((mr[0][i])**2+(mr[j][1])**2)**(1/2)
         ri = np.append(ri,rad)
-print(k,ri)
-print(np.shape(k), np.shape(ri))
+
 #Gives 4 radii but that's fine for now
 
 #Array of distances 
-x,y = np.meshgrid(np.arange(0,400,10), np.arange(0,400,10))
+x,y = np.meshgrid(np.arange(1,1000,100), np.arange(1,1000,100))
 r = ((x**2+y**2)**(1/2)).reshape(len(x)**2)
-
+x = x.reshape(100)
+y = y.reshape(100)
+theta = np.array([])
+for i in range(len(r)):
+    angle = math.acos(x[i]/r[i])
+    theta = np.append(theta,angle)
+    i = i+1
+#x = 20
+#y = 2000
+#r = ([(x**2+y**2)**(1/2)])
 #Constants
-rs = 100
-rho = 8
+rs = 450
+rho = 0.8
 beta = 1
-#Peculiar Velocity function
-def PecVel(Ri, R, w):
-    PecVel = np.array([])
-    for j in range(len(R)):
-        Inter = np.array([])
-        for i in range(len(Ri)):
-            if abs(Ri[i]-R[j])<rs:
-                SmFun = abs(Ri[i]-R[j])**3/rs**3
-            else:
-                SmFun = 1
-            print(SmFun)
-            #SmthFun = np.append(SmthFun,SmFun)
 
-            #Incorporate summation
-            Int = SmFun*(w[i]*(Ri[i]-R[j])/abs(Ri[i]-R[j])**3)
-            Inter = np.append(Inter, Int)
-        Sum = sum(Inter)
+#Smoothing function needs to be individual for each r as well as summing over the masses - need to
+#do the sum first
 
+def Sum(Ri,R,w):
+    Sum = np.array([])
+    for i in range(len(Ri)):
+        if abs(Ri[i]-R)<rs:
+            SmFun = abs(Ri[i]-R)**3/rs**3
+        else:
+            SmFun = 1
+        S = (SmFun*w[i]*(Ri[i] - R)/(abs(Ri[i]-R))**3)
+        Sum = np.append(Sum,S)
+        i = i+1
+    return(Sum)
 
-        #Finally PecVel!
-        PV = beta*((1/(4*math.pi*rho))*(Sum)+R[j]/3)
-        PecVel = np.append(PecVel, PV)
-    return(PecVel)
+def PecVel(Sum, w, R, Ri):
+    PV = np.array([])
+    for i in range(len(Ri)):
+        u = beta*(((1/(4*math.pi*rho))*sum(Sum))+R/3)
+        PV = np.append(PV, u)
+        i = i+1
+    return(PV)
 
-#Test
-Final = PecVel(ri,r,k)
-print(Final)
-print(np.shape(r))
-print(np.shape(ri))
-
+ThankyaJesus = np.array([])
+for i in range(len(r)):
+    Sumterm = Sum(ri,r[i],k)
+    Peculiar = PecVel(Sumterm, k, r[i], ri)
+    ThankyaJesus = np.append(ThankyaJesus, Peculiar)
+    i = i+1
 #Now need x and y components of the peculiar velocities
 #Since we're in the positive quadrant, can just use cos and sin?
-"""
-plt.quiver(x,y,Final,-Final)
+Pecx = np.array([])
+Pecy = np.array([])
+for i in range(len(ThankyaJesus)):
+    pecx = ThankyaJesus[i]*math.cos(theta[i])
+    pecy = ThankyaJesus[i]*math.sin(theta[i])
+    Pecx = np.append(Pecx, pecx)
+    Pecy = np.append(Pecy, pecy)
+    i = i+1
+
+
+plt.quiver(x,y,Pecx,Pecy)
 axes = plt.gca()
-axes.set_aspect(1)
+#axes.set_aspect(1)
 plt.show()
-"""
