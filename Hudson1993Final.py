bce66b839c3cda34fa7a36317865cda2b3e4e4fb
TJadeUQ <37641472+TJadeUQ@users.noreply.github.com>
2018-04-11 14:41:01 +1000

    Add files via upload

--- Hudson1993Final.py
+++ Hudson1993Final.py
@@ -14,11 +14,22 @@ from scipy import linalg as la
 import itertools as it
 #Simplify to single mass and a single vector
 #The mass of the objects w_i and their location r_i
-w_i = np.array([100,100,100])
-r_i = np.matrix([[0,0],[500,500],[200,500]])
+w_i = np.array([100])
+r_i = np.array([[250,250]])
 #Random vectors to test the field
-r = np.matrix([[50,50],[100,50],[400,400],[350,200],[300,300],[200,500],[200,200],[150,100],[-500,-500]])
-
+#r = np.array([[300,500],[50,50],[100,50],[400,400],[350,200],[300,300],[200,500],[200,200],[150,100],[300,400],[100,300],[100,400],[50,500],[150,300],[200,200],[400,100],[150,300]])
+r = np.array([[50,50],[50,100],[50,150],[50,200],[50,250],[50,300],[50,350],[50,400],[50,450],[50,500],
+              [100,50],[100,100],[100,150],[100,200],[100,250],[100,300],[100,350],[100,400],[100,450],[100,500],
+              [200,50],[200,100],[200,150],[200,200],[200,250],[200,300],[200,350],[200,400],[200,450],[200,500],
+              [300,50],[300,100],[300,150],[300,200],[300,250],[300,300],[300,350],[300,400],[300,450],[300,500],
+              [400,50],[400,100],[400,150],[400,200],[400,250],[400,300],[400,350],[400,400],[400,450],[400,500],
+              [500,50],[500,100],[500,150],[500,200],[500,250],[500,300],[500,350],[500,400],[500,450],[500,500],
+              [150,50],[150,100],[150,150],[150,200],[150,250],[150,300],[150,350],[150,400],[150,450],[150,500],
+              [250,50],[250,100],[250,150],[250,200],[250,300],[250,350],[250,400],[250,450],[250,500],
+              [350,50],[350,100],[350,150],[350,200],[350,250],[350,300],[350,350],[350,400],[350,450],[350,500],
+              [450,50],[450,100],[450,150],[450,200],[450,250],[450,300],[450,350],[450,400],[450,450],[450,500],
+              [0,50],[0,100],[0,150],[0,200],[0,250],[0,300],[0,350],[0,400],[0,450],[0,500]])
+              
 print(r)
 print(len(r_i))
 print(len(r))
@@ -58,7 +69,7 @@ def Internal(Ri,R,Smth,W):
     out = np.array([])
     for i in range(len(R)):
         for wi,ri in zip(W,Ri):
-            print(ri,wi,R[i]) #How to tell python that one is a vector and the mass is an integer
+            print(ri,wi,R[i]) 
             Int = Smth[i]*wi*(ri-R[i])/(la.norm(ri-R[i])**3)          
             print(Int)
             out = np.append(out,Int)
@@ -78,9 +89,9 @@ print('Ouput',output)
 print('Final',output[1,0])
 print(len(output))
 Final_Mix = ([])
-for i,j,k in output:
-    print('List',i,j,k)
-    fin = i + j +k
+for i in output:
+    print('List',i)
+    fin = i
     print('fin',fin)
     Final_Mix = np.append(Final_Mix,fin)
 
@@ -94,8 +105,9 @@ for i in range(len(Final_Mix)):
     Inter = beta*((1/(4*math.pi*rho)*(Final_Mix[i,:])))
     print('Inter',Inter,r[i]/3,Final_Mix[i,:])
     Sum = np.append(Sum,Inter)
-#The masses are forming the columns, rather than the x and y coordinates
+
 print('Sum',Sum)
+
 #Reshape the arrays into useable forms
 Sum = Sum.reshape(len(Final_Mix),2)
 print(len(Sum))
@@ -108,6 +120,8 @@ print('SUM',Sum)
 
 #Plot them vectors
 
+
+
 fig = plt.figure()
 plt.quiver(X,Y,Sum[:,0],Sum[:,1])
 axes = plt.gca()
@@ -117,6 +131,38 @@ plt.xlim(0,500)
 fig.suptitle('Peculiar Velocity Field')
 plt.xlabel('x coordinate')
 plt.ylabel('y coordinate')
+
+plt.show()
+
+"""""""""""""""""""""""""""""""""
+Now that we have the velocity field,
+we can start plotting
+the velocity vs distance for fixed mass
+and distance vs velocity for fixed distance
+"""""""""""""""""""""""""""""""""
+mag_rad = np.array([])
+mag_pec = np.array([])
+mag_mass = la.norm(r_i)
+#Find maagnitude of vectors
+for i,j in zip(Sum,r):
+    print(j,r_i)
+    magSum = la.norm(i)
+    magr = la.norm(j)
+    diff = la.norm(j - r_i)
+    print('Diff',diff)
+    mag_rad = np.append(mag_rad,diff)
+    mag_pec = np.append(mag_pec,magSum)
+
+print(mag_rad.shape,mag_pec.shape)
+
+#Center the graph at zero by using the differenc between the mass and the vector
+plt.scatter(mag_rad,mag_pec)
+
+fig.suptitle('Peculiar Velocity for Fixed Mass')
+plt.xlabel('Magnitude of radial coordinate')
+plt.ylabel('Magnitude of peculiar velocity')
+
+
 
 plt.show()
 
