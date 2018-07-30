a279180f6326ff1bd6aa02f172d91b2d5331b7b3
TJadeUQ <37641472+TJadeUQ@users.noreply.github.com>
2018-07-30 17:48:46 +1000

    Add files via upload

--- 3DHist.py
+++ 3DHist.py
@@ -1,19 +1,11 @@
 import numpy as np
 import matplotlib.pyplot as plt
-import matplotlib.patches as mpatches
 import math
 import scipy
-from scipy.integrate import quad
-import itertools as it
-import pandas as pd
-import cProfile
-import re
 from scipy import linalg as la
 import array
-from sympy import *
-from mpl_toolkits.mplot3d import Axes3D
-from collections import OrderedDict
-from math import floor
+from mpl_toolkits import mplot3d
+import pandas as pd
 
 
 df = pd.read_csv('Noice.csv')
@@ -37,18 +29,22 @@ L_r0 = 0.75
 L_m0 = 0.25
 L_k0 = 0.044
 L_l0 = 1
-
-
+a = 1
+f = 1
+n_bins = 4
 #Create data array
-data_array = np.array([[x_c], [y_c], [z_c]]).T
 
+data_array = np.array([[x_c], [y_c], [z_c]]).T
+print(data_array)
 data_array_M = np.array([x_c,y_c,z_c, Mass_gal]).T
-print(data_array_M)
+
 data_array_1 = np.array([x_c, y_c, z_c]).T
 #Create 3D histogram
-hist, edges = np.histogramdd(data_array_1, bins = (3,3,3))
+hist, edges = np.histogramdd(data_array_1, bins = (n_bins,n_bins,n_bins))
+print(hist)
 print(edges)
 edges = np.array(edges)
+print(len(edges), edges.shape[1])
 print(edges)
 #Use these edges to define data_array_M structure
 
@@ -58,30 +54,29 @@ def Mass_array(x_bin_1, x_bin_2, y_bin_1, y_bin_2, z_bin_1, z_bin_2, data):
     for i in range(len(data)):
         if x_bin_1< data[i,0] <= x_bin_2 and y_bin_1 < data[i,1] <= y_bin_2 and z_bin_1 < data[i,2] <= z_bin_2:
             M = np.append(M, data[i,3])
-            print(data[i,3])
         else:
             pass
     return(M)
-print('Edge length',len(edges))
-Mass = np.array([])
-for K in range(len(edges)-1):
-    for J in range(len(edges)-1):
-        for I in range(len(edges)-1):
-            print(I,J,K)
-            F = Mass_array(edges[0,K], edges[0,K+1], edges[1,J], edges[1,J+1], edges[2, I], edges[2,I+1], data_array_M)
-            print('Mass',F)
-            Mass = np.append(Mass, sum(F))
 
+Mass = np.array([])
+for K in range(len(edges[1])-1):
+    for J in range(len(edges[1])-1):
+        for I in range(len(edges[1])-1):
+            print(edges[0,K], edges[0,K+1], edges[1,J], edges[1,J+1], edges[2, I], edges[2,I+1])
+            
+            F = Mass_array(edges[0,K], edges[0,K+1], edges[1,J], edges[1,J+1], edges[2, I], edges[2,I+1], data_array_M)
+            print('Sum',F)
+            Mass = np.append(Mass, sum(F))
 
 print('FINAL',Mass)
 
-
+#Mass = Mass_gal
 #Size of bins
 def Size(bins):
     Volume = np.array([])
-    for I in range(len(bins)-1):
-        for J in range(len(bins)-1):
-            for K in range(len(bins)-1):
+    for I in range(len(bins[1])-1):
+        for J in range(len(bins[1])-1):
+            for K in range(len(bins[1])-1):
                 V = ((bins[0,I+1]-bins[0,I])*(bins[1,J+1]-bins[1,J])*(bins[2,K+1]-bins[2,K]))
                 Volume = np.append(Volume, V)
     return(Volume)
@@ -109,26 +104,99 @@ for den in Density:
     Over_Den = np.append(Over_Den, rho)
 print(Over_Den)
 
-Over_Den = np.reshape(Over_Den, np.shape(edges))
-print(Over_Den)
+Over_Den = np.reshape(Over_Den, [(len(edges[1])-1),(len(edges[1])-1),(len(edges[1])-1)])
+print('OVER',Over_Den)
+
+
 # Smooth gridded distribution
 def dosmooth(scale,datgrid,lx,ly,lz):
-  print('\nSmoothing grid with scale =',scale,'...')
+  #print('\nSmoothing grid with scale =',scale,'...')
   nx,ny,nz = datgrid.shape[0],datgrid.shape[1],datgrid.shape[2]
   norm = np.sum(datgrid)
   x = lx*np.fft.fftfreq(nx)
   y = ly*np.fft.fftfreq(ny)
   z = lz*np.fft.fftfreq(nz)
   rsqgrid = x[:,np.newaxis,np.newaxis]**2 + y[np.newaxis,:,np.newaxis]**2 + z[np.newaxis,np.newaxis,:]**2
-  kerngrid = np.exp(-rsqgrid/(2.*(scale**2)))
+  kerngrid = np.exp(-rsqgrid/(2.*(scale**2))) #Gaussian smoothing kernel
   datspec = np.fft.rfftn(datgrid)
-  kernspec = np.fft.rfftn(kerngrid)
-  datgrid = np.fft.irfftn(datspec*kernspec)
+  kernspec = np.fft.rfftn(kerngrid) #Fourier transform of smoothed kernel
+  multiply = datspec*kernspec
+  datgrid = np.fft.irfftn(multiply)
   datgrid *= norm/np.sum(datgrid)
- 
-  return datgrid
-    
-Smooth = dosmooth(1, Over_Den, len(edges[0]), len(edges[1]), len(edges[2]))
-print(Smooth)
+  return datgrid, x, y, z
 
-#Only outputting 8 masses - need to check the bins (works for (2,2,2) bins).
+
+#Smoothed 3D density field  
+Smooth, k_space_x, k_space_y, k_space_z = dosmooth(1, Over_Den, n_bins, n_bins, n_bins)
+print('K-Space',k_space_x)
+print('Smooth', Smooth, Smooth.shape)
+
+
+#Velocity field
+
+
+def Vel_General(OverDen_Gen, k_space, k_mag, a, f, H):
+    v_k = lambda k: - complex(0,1) * a * f * H * k / k_mag**2 * OverDen_Gen #Equation 5
+    v_real = np.fft.ifftn(v_k(k_space)) #Fourier transform velocity into real space
+    return v_real
+
+#Break down into components 
+
+#Calculate the magnitude of wavevector
+k_mag_x = la.norm(k_space_x)
+k_mag_y = la.norm(k_space_y)
+k_mag_z = la.norm(k_space_z)
+
+#Calculate each component       
+vel_x = Vel_General(Smooth,k_space_x, k_mag_x, 1, 1, 70)
+vel_y = Vel_General(Smooth, k_space_y, k_mag_y, 1, 1, 70)
+vel_z = Vel_General(Smooth, k_space_z, k_mag_z, 1, 1, 70)
+print('Vel',vel_x, vel_y, vel_z)
+
+#Plot the actual and calculated velocities
+fig = plt.figure()
+ax = plt.axes(projection = '3d')
+
+ax.scatter(vx, vy, vz, alpha = 0.3)
+ax.set_xlabel('X-velocity (Mpc/s?)')
+ax.set_ylabel('Y-velocity (Mpc/s?)')
+ax.set_zlabel('Z-velocity (Mpc/s?)')
+
+plt.show()
+
+ax.scatter(vel_x, vel_y, vel_z, alpha = 0.3)
+ax.set_xlabel('X-velocity (Mpc/s?)')
+ax.set_ylabel('Y-velocity (Mpc/s?)')
+ax.set_zlabel('Z-velocity (Mpc/s?)')
+plt.show()
+
+"""
+#Mid point of bins
+mid = np.array([])
+
+for K in range(len(edges[1])-1):
+    for J in range(len(edges[1])-1):
+        for I in range(len(edges[1])-1):
+            print(edges[0,K], edges[0,K+1], edges[1,J], edges[1,J+1], edges[2, I], edges[2,I+1])
+            mx = (edges[0,I+1]+edges[0,I])/2
+            my = (edges[1,J+1]+edges[1,J])/2
+            mz = (edges[2,K+1]+edges[2,K])/2
+            mid = np.append(mid, np.array([mx,my,mz]))
+            print('Midpoint',mid)
+
+mid = mid.reshape(n_bins**(3), 3)
+print(mid, mid.shape)
+
+print(mid[:,1])
+
+scatter = ax.scatter(mid[:,0], mid[:,1], mid[:,2], c = Smooth_Colour, cmap = 'viridis_r', alpha = 0.3);
+ax.set_xlabel('X-coordinates (Mpc)')
+ax.set_ylabel('Y-coordinates (Mpc)')
+ax.set_zlabel('Z-coordinates (Mpc)')
+ax.set_xlim(0,max(x_c))
+ax.set_ylim(0,max(y_c))
+ax.set_zlim(0,max(z_c))
+
+fig.colorbar(scatter)
+plt.show()
+"""
